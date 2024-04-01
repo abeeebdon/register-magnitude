286adf1db8f6b701d6e6b942905591e6af780b34
abeebdon <abeebdon@gmail.com>
2024-04-01 13:36:47 +0100

    no error

--- src/components/Guests.js
+++ src/components/Guests.js
@@ -3,16 +3,15 @@ import image2 from '../images/speaker2.jpg'
 const Guests = () => {
   return (
     <article className="font-rubik pb-8">
-      <div className="w-full">
-        <div className="w-full relative my-6 py-10 h-[100%] text-center mx-auto">
+      <div className="w-full min-[650px]:flex min-[650px]:justify-center items-center">
+        <div className="relative my-6 py-10 h-[450px] max-w-[300px] text-center mx-auto">
           <img src={image1} className="image" alt="elon" />
           <div className="bg-white p-4 w-[85%] absolute text-blue-600 text-[1.4rem] bottom-[-10%] left-0 right-0 mx-auto">
             <h2 className="font-bold">Gbolahan Alaba</h2>
             <p className="mt-4 text-[1.2rem]">Software Engineer</p>
           </div>
         </div>
-
-        <div className="w-full relative h-[100%]">
+        <div className="w-full relative my-6 py-10 h-[450px] max-w-[300px] text-center mx-auto">
           <img src={image2} className="image" alt="musk" />
           <div className="bg-white p-4 w-[85%] absolute text-blue-600 text-[1.4rem] bottom-[-10%] left-0 right-0 mx-auto">
             <h2 className="font-bold">Isaac Olowofila</h2>
--- src/sections/Section1.js
+++ src/sections/Section1.js
@@ -3,24 +3,27 @@ import React from 'react'
 const Section1 = () => {
   return (
     <div className=" my-8 p-4 mx-auto w-full max-w-[750px] relative min-[650px]:flex flex-row">
-      <div className="w-fit border-[1rem] border-white rounded-full bg-blue-950 p-6 mx-auto absolute min-[650px]:top-[0%] min-[650px]:bottom-[0%] min-[650px]:left-[50%] min-[650px]:right-[0%] top-[-17%] right-0 left-0">
-        <p className=" text-white mx-auto px-4 py-8 text-[1.2rem]">
+      <div className="w-fit border-[1.2rem] border-white rounded-full bg-blue-950 p-6 mx-auto absolute top-[-18%] right-0 left-0">
+        <p className=" text-white mx-auto font-bold px-4 py-10 text-[2rem]">
           The Youth <br />
           Social Summit
           <br /> Nigeria
         </p>
       </div>
-      <div className=" bg-blue-950 text-white pt-10 p-4 mt-12 ">
-        <p className="w-full max-w-[650px] mx-auto px-[1rem] pt-[5rem]">
-          "Welcome to our exciting event! We're thrilled to have you join us for
-          an unforgettable experience filled with inspiration, networking, and
-          fun. Get ready to immerse yourself in a day of engaging activities,
-          enlightening discussions, and opportunities to connect with fellow
-          attendees. From insightful keynote speakers to interactive workshops,
-          this event promises to broaden your horizons and leave you feeling
-          energized. So, sit back, relax, and enjoy every moment as we embark on
-          this journey together. Welcome, and let the adventure begin!"
-        </p>
+      <div className=" bg-blue-950 text-white pt-14 pb-8 p-4 mt-[4rem] ">
+        <div className="mt-8">
+          <p className="w-full max-w-[700px] mx-auto px-[1rem] pt-[5rem] text-[1.2rem]">
+            "Welcome to our exciting event! We're thrilled to have you join us
+            for an unforgettable experience filled with inspiration, networking,
+            and fun. Get ready to immerse yourself in a day of engaging
+            activities, enlightening discussions, and opportunities to connect
+            with fellow attendees. From insightful keynote speakers to
+            interactive workshops, this event promises to broaden your horizons
+            and leave you feeling energized. So, sit back, relax, and enjoy
+            every moment as we embark on this journey together. Welcome, and let
+            the adventure begin!"
+          </p>
+        </div>
       </div>
     </div>
   )
