75d41000b66fd5310c12f748f0a1aa0ef18c7ec2
abeebdon <abeebdon@gmail.com>
2024-04-02 18:21:38 +0100

    ERR

--- src/sections/Section1.js
+++ src/sections/Section1.js
@@ -2,27 +2,29 @@ import React from 'react'
 
 const Section1 = () => {
   return (
-    <div className=" my-8 p-4 mx-auto w-full max-w-[750px] relative sm:flex flex-row">
-      <div className="w-fit border-[1rem] border-white rounded-full bg-blue-950 p-6 mx-auto absolute top-[-18%] right-0 left-0">
-        <p className=" text-white mx-auto font-bold px-4 py-10 text-[1.4rem] sm:text-[2rem]">
-          The Youth <br />
-          Social Summit
-          <br /> Nigeria
-        </p>
-      </div>
-      <div className=" bg-blue-950 text-white pt-14 pb-8 p-4 mt-[4rem] ">
-        <div className="mt-8">
-          <p className="w-full max-w-[700px] mx-auto px-[1rem] pt-[1.4rem] sm:pt-[5rem] text-[1.2rem]">
-            "Welcome to our exciting event! We're thrilled to have you join us
-            for an unforgettable experience filled with inspiration, networking,
-            and fun. Get ready to immerse yourself in a day of engaging
-            activities, enlightening discussions, and opportunities to connect
-            with fellow attendees. From insightful keynote speakers to
-            interactive workshops, this event promises to broaden your horizons
-            and leave you feeling energized. So, sit back, relax, and enjoy
-            every moment as we embark on this journey together. Welcome, and let
-            the adventure begin!"
+    <div className="bg-gradient-to-br from-white to-blue-200">
+      <div className="max-w-[1100px] mx-auto flex flex-col relative ">
+        <div className="w-fit border-[1rem] border-white rounded-full bg-blue-950 p-4 mx-auto my-4 sm:h-fit lg:absolute top-[10%]">
+          <p className=" text-white mx-auto font-bold px-4 py-10 text-[1.4rem] sm:text-[1.75rem]">
+            The Youth <br />
+            Social Summit
+            <br /> Nigeria
           </p>
+        </div>
+        <div className=" bg-blue-950 p-6 text-white md:w-[80%] lg:mt-10 max-w-[900px] lg:text-left mx-auto lg:pt-8 lg:mr-[2%] ">
+          <div className="mt-4 lg:mt-6 lg:pt-4 lg:pb-8 lg:ml-10 lg:pl-10">
+            <h2 className=" text-[1.9rem] lg:text-[2.2rem] font-bold">
+              The Youth Summit 1.0
+            </h2>
+            <p className="w-full mt-2 text-[1rem] lg:text-xl">
+              Welcome to our exciting event! We're thrilled to have you join us
+              for an unforgettable experience filled with inspiration,
+              networking, and fun. Get ready to immerse yourself in a day of
+              engaging activities, enlightening discussions, and opportunities
+              to connect with fellow attendees. From insightful keynote speakers
+              to interactive workshops.
+            </p>
+          </div>
         </div>
       </div>
     </div>
