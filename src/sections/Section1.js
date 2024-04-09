eb2c7b9e1797669cac801b62deacb24c92689cfe
abeebdon <abeebdon@gmail.com>
2024-04-09 18:41:11 +0100

    remaining image

--- src/sections/Section1.js
+++ src/sections/Section1.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Section1 = () => {
   return (
-    <div className="bg-gradient-to-br from-white to-blue-200">
+    <div className="bg-blue-300 sm:bg-gradient-to-br from-white to-blue-200">
       <div className="max-w-[1100px] mx-auto flex flex-col relative ">
         <div className="w-fit border-[1rem] border-white rounded-full bg-blue-950 p-4 mx-auto my-4 sm:h-fit lg:absolute top-[10%]">
           <p className=" text-white mx-auto font-bold px-4 py-10 text-[1.4rem] sm:text-[1.75rem]">
@@ -11,7 +11,7 @@ const Section1 = () => {
             <br /> Nigeria
           </p>
         </div>
-        <div className="bg-blue-950 mx-4 p-6 text-white md:w-[80%] lg:mt-10 max-w-[900px] lg:text-left sm:mx-auto lg:pt-8 lg:mr-[2%] ">
+        <div className="sm:bg-blue-950  p-6 text-white md:w-[80%] lg:mt-10 max-w-[900px] lg:text-left sm:mx-auto lg:pt-8 lg:mr-[2%] ">
           <div className="mt-4 lg:mt-6 lg:pt-4 lg:pb-8 lg:ml-10 lg:pl-10">
             <h2 className=" text-[1.9rem] lg:text-[2.2rem] font-bold">
               The Youth Summit 1.0
--- src/sections/Section2.js
+++ src/sections/Section2.js
@@ -40,7 +40,7 @@ const Section2 = () => {
   }, [countdown.days])
 
   return (
-    <section className="font-rubik bg-blue-950 text-white p-8 m-4">
+    <section className="font-rubik bg-blue-950 text-white p-8 sm:m-4">
       <div className="sm:flex max-[500px]:flex-col justify-between items-center max-w-[1000px] mx-auto">
         <h2 className="font-bold text-[3rem] text-center">Event Countdown</h2>
         <div className="flex justify-between items-center gap-[1rem] min-[800]:gap-[6%]">
