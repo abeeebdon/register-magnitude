d8b2d956f7512a5ea84ca4a6279512c72c2c4923
abeebdon <abeebdon@gmail.com>
2024-04-09 23:26:35 +0100

    err

--- src/sections/Guests.js
+++ src/sections/Guests.js
@@ -11,7 +11,7 @@ const Guests = () => {
             <p className="mt-4 text-[1.2rem]">Software Engineer</p>
           </div>
         </div>
-        <div className="w-full relative my-6 py-10 h-[400px] max-w-[300px] text-center mx-auto">
+        <div className="w-full relative my-6 py-10 h-[450px] max-w-[300px] text-center mx-auto">
           <img src={image2} className="image" alt="musk" />
           <div className="bg-white p-4 w-[85%] absolute text-blue-600 text-[1.4rem] bottom-[-10%] left-0 right-0 mx-auto">
             <h2 className="font-bold">Isaac Olowofila</h2>
