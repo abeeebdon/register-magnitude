20921e14bd2ec57c615bae41759a9da968d93032
abeebdon <abeebdon@gmail.com>
2024-04-19 22:37:51 +0100

    deployment

--- src/sections/Section.js
+++ src/sections/Section.js
@@ -1,10 +1,21 @@
-import image2 from '../images/NYSC.jpg'
 const Section = () => {
   return (
     <div
-      className="h-[350px] sm:h-[70vh] bg-cover"
-      style={{ backgroundImage: 'url("/NYSC.jpg")' }}
-    ></div>
+      className=" h-[350px] sm:h-[70vh] bg-cover font-arial "
+      style={{
+        backgroundImage:
+          'linear-gradient(to right, rgba(100,140,130,0.65), rgba(70,100,50,0.6)), url("/NYSC.jpg")',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+      }}
+    >
+      <div className="py-10">
+        <p className=" text-white mx-auto text-center  font-bold px-4 py-10 text-[5rem] sm:text-[5.2rem]">
+          THE YOUTH <p className="text-[3.7rem]"> SOCIAL SUMMIT </p>
+          <p className="text-[5rem]">NIGERIA 1.0 </p>
+        </p>
+      </div>
+    </div>
   )
 }
 
