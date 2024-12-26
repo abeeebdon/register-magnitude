cc25c3f8b6a2ff103f8b5a4dd95e8882c9c8b919
abeebdon <abeebdon@gmail.com>
2024-12-26 22:08:12 +0100

    Fixing 500 and 502 erroes

--- src/signup/Signup.js
+++ src/signup/Signup.js
@@ -9,7 +9,7 @@ import { CgSpinner } from 'react-icons/cg'
 const validationSchema = yup.object().shape({
   firstname: yup.string().required('First name is required'),
   lastname: yup.string().required('Last name is required'),
-  phoneNumber: yup
+  phone: yup
     .string()
     .matches(/^\d+$/, 'Phone number must be numeric')
     .required('Phone number is required'),
@@ -18,7 +18,6 @@ const validationSchema = yup.object().shape({
     .email('Invalid email format')
     .required('Email is required'),
   location: yup.string().required('Location is required'),
-  gender: yup.string().required('Gender is required'),
   skills: yup.string().required('Skills are required'),
 })
 
@@ -27,6 +26,7 @@ const Signup = ({ setSuccess }) => {
   const [isLoading, setIsLoading] = useState(false)
   const [isError, setIsError] = useState(false)
   const [optionValue, setOptionValue] = useState('')
+  const [gender, setGender] = useState(true)
 
   const URL = 'https://twocdc-conference.onrender.com/reg/register/'
 
@@ -41,12 +41,17 @@ const Signup = ({ setSuccess }) => {
   })
 
   const onSubmit = async (data) => {
+    console.log(data)
     setIsLoading(true)
     setIsError(false)
     setErrMsg('')
 
     try {
-      const response = await axios.post(URL, { ...data, heard_by: optionValue })
+      const response = await axios.post(URL, {
+        ...data,
+        gender: gender,
+        heard_by: optionValue,
+      })
       if (response.status === 201) {
         setIsLoading(false)
         setSuccess(true)
@@ -73,181 +78,169 @@ const Signup = ({ setSuccess }) => {
         <div>
           <p>Register to attend the 2CDC Conference</p>
         </div>
-        <SignupFor
-          onSubmit={handleSubmit(onSubmit)}
-          register={register}
-          isLoading={isLoading}
-          isError={isError}
-          errMsg={errMsg}
-          errors={errors}
-          handleOptionChange={handleOptionChange}
-        />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* First name and  last name */}
+          <div className="pr-4 w-full max-w-[550px] mx-auto justify-between">
+            <div className="mx-2 my-4 w-full">
+              <input
+                placeholder="First Name"
+                {...register('firstname')}
+                className="input"
+              />
+              {errors.firstname && (
+                <p className="errors">{errors.firstname.message}</p>
+              )}
+            </div>
+            <div className="mx-2 my-4 w-full">
+              <input
+                {...register('lastname')}
+                className="input"
+                placeholder="Last Name"
+              />
+              {errors.lastname && (
+                <p className="errors">{errors.lastname.message}</p>
+              )}
+            </div>
+          </div>
+
+          {/* Email and Password  */}
+          <div className="pr-4 w-full max-w-[550px] mx-auto justify-between">
+            <div className="mx-2 my-4 w-full">
+              <input
+                {...register('email')}
+                className="input"
+                placeholder="Email Address"
+              />
+              {errors.email && <p className="errors">{errors.email.message}</p>}
+            </div>
+
+            <div className="mx-2 my-4 w-full">
+              <input
+                {...register('phone')}
+                className="input"
+                placeholder="Phone Number"
+              />
+              {errors.phoneNumber && (
+                <p className="errors">{errors.phoneNumber.message}</p>
+              )}
+            </div>
+          </div>
+          <div className="pr-4 w-full max-w-[550px] mx-auto justify-between">
+            <div className="p-2 text-left flex">
+              <label className="block">Gender:</label>
+              <div className="px-2">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="true"
+                  checked={gender === 'true'}
+                  onChange={(e) => setGender(e.target.value)}
+                />
+                <span className="mx-2">Male</span>
+              </div>
+              <div className="px-2">
+                <input
+                  type="radio"
+                  name="gender"
+                  value="false"
+                  checked={gender === 'false'}
+                  onChange={(e) => setGender(e.target.value)}
+                />
+                <span className="mx-2">Female</span>
+              </div>
+            </div>
+
+            {/* Skills Checkboxes */}
+            <div className="mx-2 my-4 w-full text-left">
+              <label className="block">Do you have any technical skills?</label>
+              <div className="flex items-center space-x-4">
+                <label>
+                  <input
+                    type="radio"
+                    value="true"
+                    {...register('skills')}
+                    className="mr-2"
+                  />
+                  Yes
+                </label>
+                <label>
+                  <input
+                    type="radio"
+                    value="false"
+                    {...register('skills')}
+                    className="mr-2"
+                  />
+                  No
+                </label>
+              </div>
+              {errors.skills && (
+                <p className="errors">{errors.skills.message}</p>
+              )}
+            </div>
+          </div>
+
+          <div className="m-2 text-left p-2  mx-auto max-w-[550px] justify-start items-center gap-6">
+            <label className="block w-fit">How did you hear about us?</label>
+
+            <select
+              className="outline-none w-full p-2"
+              onChange={handleOptionChange}
+            >
+              <option className="option-input" value="">
+                Select an option
+              </option>
+
+              <option className="option-input" value="Google">
+                Google
+              </option>
+              <option className="option-input" value="Facebook">
+                Facebook
+              </option>
+              <option className="option-input" value="Linkedin">
+                Linkedin
+              </option>
+              <option className="option-input" value="Instagram">
+                Instagram
+              </option>
+              <option className="option-input" value="Friends">
+                Friends
+              </option>
+              <option className="option-input" value="Others">
+                Others
+              </option>
+            </select>
+          </div>
+          <div className="p-2 max-w-[550px] mx-auto">
+            <input
+              {...register('location')}
+              className="input"
+              placeholder="Please Enter Your Location"
+            />
+            {errors.location && (
+              <p className="errors">{errors.location.message}</p>
+            )}
+          </div>
+          {isError && <p className="errors text-center">{errMsg}</p>}
+          <div className="mx-auto m-4 pb-10 w-[100%] max-w-[550px] relative">
+            <button
+              type="submit"
+              className="bg-blue-950 p-3 rounded-lg text-white text-center font-semibold w-full outline-none sm:max-w-[200px] sm:absolute right-0"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <CgSpinner
+                  className="animate-spin text-[1.4rem] w-full mx-auto"
+                  size={24}
+                />
+              ) : (
+                'Register'
+              )}
+            </button>
+          </div>
+        </form>
       </div>
     </section>
   )
 }
 
 export default Signup
-
-const SignupFor = ({
-  onSubmit,
-  register,
-  isLoading,
-  isError,
-  errMsg,
-  errors,
-  handleOptionChange,
-}) => (
-  <form onSubmit={onSubmit}>
-    {/* First name and  last name */}
-    <div className="pr-4 w-full max-w-[550px] mx-auto justify-between">
-      <div className="mx-2 my-4 w-full">
-        <input
-          placeholder="First Name"
-          {...register('firstname')}
-          className="input"
-        />
-        {errors.firstname && (
-          <p className="errors">{errors.firstname.message}</p>
-        )}
-      </div>
-      <div className="mx-2 my-4 w-full">
-        <input
-          {...register('lastname')}
-          className="input"
-          placeholder="Last Name"
-        />
-        {errors.lastname && <p className="errors">{errors.lastname.message}</p>}
-      </div>
-    </div>
-
-    {/* Email and Password  */}
-    <div className="pr-4 w-full max-w-[550px] mx-auto justify-between">
-      <div className="mx-2 my-4 w-full">
-        <input
-          {...register('email')}
-          className="input"
-          placeholder="Email Address"
-        />
-        {errors.email && <p className="errors">{errors.email.message}</p>}
-      </div>
-
-      <div className="mx-2 my-4 w-full">
-        <input
-          {...register('phoneNumber')}
-          className="input"
-          placeholder="Phone Number"
-        />
-        {errors.phoneNumber && (
-          <p className="errors">{errors.phoneNumber.message}</p>
-        )}
-      </div>
-    </div>
-    <div className="pr-4 w-full max-w-[550px] mx-auto justify-between">
-      <div className="mx-2 my-4 text-left w-full">
-        <label className="block">Gender:</label>
-        <div className="flex items-center space-x-4">
-          <label>
-            <input
-              type="radio"
-              value="male"
-              {...register('gender')}
-              className="mr-2"
-            />
-            Male
-          </label>
-          <label>
-            <input
-              type="radio"
-              value="female"
-              {...register('gender')}
-              className="mr-2"
-            />
-            Female
-          </label>
-        </div>
-        {errors.gender && <p className="errors">{errors.gender.message}</p>}
-      </div>
-
-      {/* Skills Checkboxes */}
-      <div className="mx-2 my-4 w-full text-left">
-        <label className="block">Do you have any technical skills?</label>
-        <div className="flex items-center space-x-4">
-          <label>
-            <input
-              type="radio"
-              value="true"
-              {...register('skills')}
-              className="mr-2"
-            />
-            Yes
-          </label>
-          <label>
-            <input
-              type="radio"
-              value="false"
-              {...register('skills')}
-              className="mr-2"
-            />
-            No
-          </label>
-        </div>
-        {errors.skills && <p className="errors">{errors.skills.message}</p>}
-      </div>
-    </div>
-
-    <div className="m-2 text-left p-2  mx-auto max-w-[550px] justify-start items-center gap-6">
-      <label className="block w-fit">How did you hear about us?</label>
-
-      <select className="outline-none w-full p-2" onChange={handleOptionChange}>
-        <option className="option-input" value="">
-          Select an option
-        </option>
-
-        <option className="option-input" value="Google">
-          Google
-        </option>
-        <option className="option-input" value="Facebook">
-          Facebook
-        </option>
-        <option className="option-input" value="Linkedin">
-          Linkedin
-        </option>
-        <option className="option-input" value="Instagram">
-          Instagram
-        </option>
-        <option className="option-input" value="Friends">
-          Friends
-        </option>
-        <option className="option-input" value="Others">
-          Others
-        </option>
-      </select>
-    </div>
-    <div className="p-2 max-w-[550px] mx-auto">
-      <input
-        {...register('location')}
-        className="input"
-        placeholder="Please Enter Your Location"
-      />
-      {errors.location && <p className="errors">{errors.location.message}</p>}
-    </div>
-    {isError && <p className="errors text-center">{errMsg}</p>}
-    <div className="mx-auto m-4 pb-10 w-[100%] max-w-[550px] relative">
-      <button
-        type="submit"
-        className="bg-blue-950 p-3 rounded-lg text-white text-center font-semibold w-full outline-none sm:max-w-[200px] sm:absolute right-0"
-        disabled={isLoading}
-      >
-        {isLoading ? (
-          <CgSpinner
-            className="animate-spin text-[1.4rem] w-full mx-auto"
-            size={24}
-          />
-        ) : (
-          'Register'
-        )}
-      </button>
-    </div>
-  </form>
-)
---
+++ src/signup/SignupForm.js
@@ -0,0 +1,178 @@
+import { CgSpinner } from 'react-icons/cg'
+
+const SignupForm = ({
+  userDetails,
+  handleSubmit,
+  setUserDetails,
+  isLoading,
+  firstnameError,
+  lastnameError,
+  emailError,
+  errMsg,
+  isError,
+  locationError,
+  genderError,
+  checkNumber,
+  skillError,
+  handleOptionChange,
+}) => {
+  return (
+    <form onSubmit={(e) => handleSubmit(e)} className="">
+      {/* for names */}
+      <div className="pr-4 w-full max-w-[550px] mx-auto justify-between">
+        <div className="m-2 my-4 w-full">
+          <input
+            type="text"
+            id="Firstname"
+            placeholder="First Name"
+            value={userDetails.firstname}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, firstname: e.target.value })
+            }
+            className="input"
+          />
+          {firstnameError && <p className="error">This is a required field</p>}
+        </div>
+        <div className="mx-2 my-4 w-full">
+          <input
+            type="text"
+            id="Lastname"
+            placeholder="Last Name"
+            value={userDetails.lastname}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, lastname: e.target.value })
+            }
+            className="input"
+          />
+          {lastnameError && <p className="error">This is a required field</p>}
+        </div>
+      </div>
+      {/* for email and phone number */}
+      <div className="pr-4 w-full max-w-[550px] mx-auto justify-between">
+        <div className="mx-2 my-4 w-full">
+          <input
+            type="text"
+            id="email"
+            placeholder="Email Address"
+            value={userDetails.email}
+            onChange={(e) =>
+              setUserDetails({ ...userDetails, email: e.target.value })
+            }
+            className="input"
+          />
+          {emailError && <p className="error">Please enter a valid email</p>}
+        </div>
+        <div className="mx-2 my-4 w-full">
+          <input
+            type="text"
+            id="number"
+            placeholder="Phone Number"
+            value={userDetails.phoneNumber}
+            onChange={(e) => {
+              checkNumber(e.target.value)
+            }}
+            className="input"
+          />
+        </div>
+      </div>
+      {/* for Gender and technical skills */}
+      <div className="pr-4 w-full max-w-[550px] mx-auto justify-between">
+        <div className="m-2 text-left  flex">
+          <label className="block">Do you have any technical skills?</label>
+
+          <div className="px-2">
+            <input
+              type="checkbox"
+              name="skills"
+              value="true"
+              checked={userDetails.skills === 'true'}
+              onChange={(e) =>
+                setUserDetails({ ...userDetails, skills: e.target.value })
+              }
+            />
+            <span className="md:mx-2">Yes</span>
+          </div>
+          <div className="px-2">
+            <input
+              type="checkbox"
+              name="skills"
+              value="false"
+              checked={userDetails.skills === 'false'}
+              onChange={(e) =>
+                setUserDetails({ ...userDetails, skills: e.target.value })
+              }
+            />
+            <span className="sm:mx-2">No</span>
+          </div>
+        </div>
+      </div>
+      <div className="mx-4">
+        {skillError && genderError && (
+          <p className=" text-red-500 text-left">This is a required field</p>
+        )}
+      </div>
+      {/* How did you hear about us */}
+      <div className="m-2 text-left p-2  mx-auto max-w-[550px] justify-start items-center gap-6">
+        <label className="block w-fit">How did you hear about us?</label>
+
+        <select
+          className="outline-none w-full p-2"
+          onChange={handleOptionChange}
+        >
+          <option className="option-input" value="">
+            Select an option
+          </option>
+
+          <option className="option-input" value="Google">
+            Google
+          </option>
+          <option className="option-input" value="Facebook">
+            Facebook
+          </option>
+          <option className="option-input" value="Linkedin">
+            Linkedin
+          </option>
+          <option className="option-input" value="Instagram">
+            Instagram
+          </option>
+          <option className="option-input" value="Friends">
+            Friends
+          </option>
+          <option className="option-input" value="Others">
+            Others
+          </option>
+        </select>
+      </div>
+      <div className="p-2 max-w-[550px] mx-auto">
+        <input
+          type="text"
+          id="location"
+          value={userDetails.location}
+          onChange={(e) =>
+            setUserDetails({ ...userDetails, location: e.target.value })
+          }
+          placeholder="Please Enter Your Location"
+          className="input"
+        />
+        {locationError && <p className="error">This is a required field</p>}
+      </div>
+      {isError && (
+        <div className="text-red-400 text-[1rem] m-2">
+          <p>{errMsg}</p>
+        </div>
+      )}
+      <div className="mx-auto m-4 pb-10 w-[100%] max-w-[550px] relative">
+        {isLoading ? (
+          <div className="bg-blue-950 p-3 rounded-lg text-white font-semibold w-full outline-none sm:w-[200px] text-center sm:absolute right-0">
+            <CgSpinner className="animate-spin text-[1.4rem] w-full mx-auto" />
+          </div>
+        ) : (
+          <button className="bg-blue-950 p-3 rounded-lg text-white font-semibold w-full outline-none sm:max-w-[200px] sm:absolute right-0">
+            Register Here
+          </button>
+        )}
+      </div>
+    </form>
+  )
+}
+export default SignupForm
